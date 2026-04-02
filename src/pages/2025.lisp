819b40258f6950ccdc9a2e50a02c1dcb3e3bb345
Julien-Gio <juliengiovinazzo@gmail.com>
2026-04-03 00:12:12 +0200

    wip edition page macro

--- src/pages/2025.lisp
+++ src/pages/2025.lisp
@@ -1,6 +1,11 @@
 (in-package #:pages)
 
 
-(def-edition-page page-2025 "2025")
+(def-edition-page page-2025 
+  (title "2025")
+  (layout
+    (header "Podium")
+    (podium)
+    (header "End of page")))
 
 
