acb08060726519aa8a104a8501ff7d9f51adfec9
Julien Giovinazzo <julien.giovinazzo@external.stellantis.com>
2026-08-31 08:57:21 +0200

    feat(html): handle tag attributes

--- src/html/renderer.lisp
+++ src/html/renderer.lisp
@@ -12,17 +12,29 @@
         ((keywordp (first node)) (write-tag node out))
         (T (loop for sub-node in node do (write-node sub-node out)))))
 
-(defun write-tag (node out) 
-  (let ((tag (string-downcase (symbol-name (first node))))
-        (children (rest node)))
-    (if (and (null children) (member tag *void-elements* :test #'string=)) 
-          (format out "<~a/>" tag)
-          (progn (format out "<~a>" tag)
+(defun write-tag (node out)
+  (let* ((tag (string-downcase (symbol-name (car node))))
+        (attributes (if (and (listp (cadr node)) (keywordp (caadr node)) (string= (symbol-name (caadr node)) "ATTRS")) (cadr node)))
+        (children (if (null attributes) (cdr node) (cddr node))))
+    (if (member tag *void-elements* :test #'string=)
+          (progn ; Void element <xxx/>
+                 (format out "<~a" tag)
+                 (write-attrs attributes out)
+                 (format out "/>"))
+          (progn ; Standard element <xxx>...</xxx>
+                 (format out "<~a" tag)
+                 (write-attrs attributes out)
+                 (format out ">")
                  (loop for child in children do (write-node child out))
                  (format out "</~a>" tag)))))
 
+(defun write-attrs (attrs out)
+  ; the first element in attrs is always ":attrs", skip it
+  (loop for (key val) on (rest attrs) by #'cddr
+        do (format out " ~a=\"~a\"" (string-downcase (symbol-name key)) (escape-html val))))
+
 (defun escape-html (in)
-  (with-output-to-string (out) 
+  (with-output-to-string (out)
     (loop for ch across in
           for escaped = (case ch (#\& "&amp;") (#\< "&lt;") (#\> "&gt;") (#\" "&quot;") (T (string ch)))
           do (write-string escaped out))))
