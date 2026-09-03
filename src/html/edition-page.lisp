aff6690774e38d40fe3f2657d52397d7f74f25f2
Julien-Gio <juliengiovinazzo@gmail.com>
2026-09-03 22:23:34 +0200

    feat: display rest of standings below podium

--- src/html/edition-page.lisp
+++ src/html/edition-page.lisp
@@ -5,30 +5,35 @@
          (title (second title-form))
          (layout-form (get-directive body 'layout))
          (layout-items (rest layout-form))
-         (use-form (get-directive body 'use ))
+         (use-form (get-directive body 'use))
          (edition-filename (second use-form)))
     `(defun ,name ()
        (let ((edition (interpreter:load-content ,edition-filename)))
          (render-html (list :html (list :head (list :title ,title)
-                                              (list :link '(:attrs :href "/assets/edition.css" :rel "stylesheet"))) 
-                                  (list :body (list :h1 "Edition header " (edition-name edition)) 
-                                              ,@layout-items)))))))
+                                        (list :link '(:attrs :href "/assets/edition.css" :rel "stylesheet")))
+                            (list :body (list :h1 "Edition header " (edition-name edition))
+                                  ,@layout-items)))))))
 
 (defmacro header (content)
   `(list :h2 ,content))
 
 (defmacro podium ()
   `(list :div '(:attrs :class "podium")
-      (list :div '(:attrs :class "podium-top-3")
-                 (list :div '(:attrs :class "silver")
-                            (participant-ref-name (second (edition-standing edition)))
-                            (list :div '(:attrs :class "podium-step")))
-                 (list :div '(:attrs :class "gold")
-                            (participant-ref-name (first (edition-standing edition)))
-                            (list :div '(:attrs :class "podium-step")))
-                 (list :div '(:attrs :class "bronze")
-                            (participant-ref-name (third (edition-standing edition)))
-                            (list :div '(:attrs :class "podium-step"))))))
+         (list :div '(:attrs :class "podium-top-3")
+               (list :div '(:attrs :class "silver")
+                     (participant-ref-name (second (edition-standing edition)))
+                     (list :div '(:attrs :class "podium-step")))
+               (list :div '(:attrs :class "gold")
+                     (participant-ref-name (first (edition-standing edition)))
+                     (list :div '(:attrs :class "podium-step")))
+               (list :div '(:attrs :class "bronze")
+                     (participant-ref-name (third (edition-standing edition)))
+                     (list :div '(:attrs :class "podium-step"))))
+         (append (list :div '(:attrs :class "rest-of-standings"))
+           (loop for p in (cdddr (edition-standing edition))
+                 for i from 4
+                 collect (list :p (list :attrs :data-position (format nil "~d" i))
+                               (participant-ref-name p))))))
 
 (defmacro trophies ()
   `(list :p "TROPHIES TODO"))
@@ -37,6 +42,6 @@
 ; =================  Helper functions  =================
 
 (defun get-directive (directives directive-symbol-name)
-  (assoc directive-symbol-name directives 
-         :key #'symbol-name 
+  (assoc directive-symbol-name directives
+         :key #'symbol-name
          :test #'string=))
