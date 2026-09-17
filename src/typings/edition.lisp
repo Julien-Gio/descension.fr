7c8866fa9e88484c6d1c5dd8063a37d976a7328a
Julien Giovinazzo <julien.giovinazzo@external.stellantis.com>
2026-09-17 19:31:25 +0200

    fix leaderboard not sorting positions correctly

--- src/typings/edition.lisp
+++ src/typings/edition.lisp
@@ -17,14 +17,17 @@
   (string= (participant-ref-name a) (participant-ref-name b)))
 
 (defun get-edition-standing (edition)
-  (let ((scores (loop with previous = nil
-                      for p in (edition-participants edition)
-                      for i = 1 then (+ i 1)
+  (let ((scores (loop for p in (edition-participants edition)
                       for score = (participant-points-in-edition edition p)
-                      for position = (if (and (second previous) (= score (second previous)))
-                                         (third previous)
-                                         i)
-                      collect (list p score position)
-                      do (setf previous (list p score i)))))
-    (sort scores #'= :key #'second)
-    scores))
+                      collect (list p score))))
+    (sort scores #'> :key #'second)
+    ; Assign positions to each ranking, and handle ties.
+    (loop with previous = nil
+          for score in scores
+          for i = 1 then (+ i 1)
+          for position = (if (and (second previous) (= (second score) (second previous)))
+                             (third previous)
+                             i)
+          for result = (append score (list position))
+          collect result
+          do (setf previous result))))
